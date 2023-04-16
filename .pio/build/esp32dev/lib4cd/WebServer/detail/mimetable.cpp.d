2213cb68f6ce02ec5e88b2fbcd9b2e7fe3697ddf
Michael Albrecht <malbrech@hm.edu>
2023-04-16 15:07:17 +0200

    Deletet Arduino BLE lib out of platformio.ini

---
+++ .pio/build/esp32dev/lib4cd/WebServer/detail/mimetable.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32dev/lib4cd/WebServer/detail/mimetable.cpp.o: \
+ /Users/michael/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
+ /Users/michael/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
+ /Users/michael/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
